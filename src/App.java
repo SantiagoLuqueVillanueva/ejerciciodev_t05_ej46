453b49944e3b7f4347f1ba90b8795ffac1f62962
Santiago Luque Villanueva <luquevillanuevasantiago@gmail.com>
2025-11-15 15:02:57 +0100

    Done

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int ancho = Integer.parseInt(System.console().readLine("Por favor, introduzca la anchura del rectángulo (como mínimo 2): "));
+        int altura = Integer.parseInt(System.console().readLine("Ahora introduzca la altura (como mínimo 2): "));
+
+        if (ancho < 2 || altura < 2) {
+            System.out.println("Lo siento, los datos introducidos no son correctos, el valor mínimo para la anchura y la altura es 2.");
+        }
+
+        for (int i = 0; i < altura; i++) {
+            for (int j = 0; j < ancho; j++) {
+                if (i == 0 || i == altura - 1 || j == 0 || j == ancho - 1)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 }
